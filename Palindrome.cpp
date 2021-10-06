e3a1b27664c6d5fd05ed67ab205a1425c51b1647
U-BSD\417197 <417197@SHS-10DD3BE2E.beaverton.k12.or.us>
2021-10-06 11:55:56 -0700

    Final Palindrome

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,42 +1,63 @@
+//Author: Nathan Zou
+//Date: 10/6/21
+//Code takes a string input from user and checks if it is a palindrome
+//Memset help from Nikhil Nayak
+//ASCII conversion idea from Chris Zou
 #include <iostream> 
 #include <cstring>
 using namespace std;
 
 int main() {
-  char input[81];
-  char converter[81];
-  char checker[81];
-  int convertercounter = 0;
-  int finalcheck = 0;
-  cout << "Enter a string" << endl;
+  char input[81]; //Input string  
+  char str[81]; //Edited input string
+  char output[81]; //Reversed input string
+  int count = 0;
+  memset(input, 0, 81); //Clear memory for input
+  memset(str, 0, 81); //Clear memory for str
+  memset(output, 0, 81); //Clear memory for ouput
+
+  //Initialize strings
+  for (int i = 0; i < 80; i++) {
+    input[i] = 0;
+    str[i] = 0;
+    output[i] = 0;
+  }
+
+  //Read in input from user
+  cout << "Enter a string." << endl;
   cin.get(input, 81);
+  cin.get();
 
-  for (int i = 0; i < strlen(input); i++) {
-    if (isalpha(input[i]) != 0) {
-      converter[convertercounter] = toupper(input[i]);
-      convertercounter++;
+  //Edit the input string and put it into str (punctionation removed)
+  for (int i = 0; i < 80; i++) {
+    //if uppercase put into str
+    if (input[i] >= 65 && input[i] <= 90) {
+      str[count] = input[i];
+      count++;
+    }
+    //Convert lowercase into uppercase (-32 from ASCII value) then put into str
+    else if (input[i] >= 97 && input[i] <= 122) {
+      str[count] = input[i] - 32;
+      count++;
     }
   }
 
-  for (int i = 0; i < strlen(converter); i++) {
-    checker[i] = converter[strlen(converter)-1-i];
+  count--;
+
+  //Run through str backwards and put it into output
+  for (int i = 0; i < 80; i++) {
+    if (str[count-i] != 0) { //If str is not null add to output
+      output[i] = str[count-i];
+    }
   }
 
-   for (int i = 0; i < strlen(converter); i++) {
-     if (checker[i] == converter[i]) {
-	finalcheck++;
-      }
-    }
+  //Compare strings
+  if (strcmp(str, output) == 0) {
+    cout << "Is a Palindrome." << endl;
+  }
+  else {
+    cout << "Is NOT a Palindrome." << endl;
+  }
 
-    if (finalcheck == strlen(converter)) {
-      cout << "PALINDROME. \n";
-      cout << "Your string after conversion. \n";
-      cout << converter;
-    }
-    else {
-      cout << "NOT a PALINDROME. \n";
-      cout << "Your string after conversion. \n";
-      cout << converter;
-    }
-    return 0;
+  return 0; 
 }
